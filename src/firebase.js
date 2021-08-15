6f444a06ce08a2fadc02bb111920b684a2752132
khaled taha <khaled.devp@gmail.com>
2021-08-15 21:15:50 +0300

    fixes

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_apiKey,
-	authDomain: process.env.REACT_APP_authDomain,
-	projectId: process.env.REACT_APP_projectId,
-	storageBucket: process.env.REACT_APP_storageBucket,
-	messagingSenderId: process.env.REACT_APP_messagingSenderId,
-	appId: process.env.REACT_APP_appId,
+	apiKey: "AIzaSyC9k3Ow_KSemh21PC2K086EYaPfSxBAL-o",
+	authDomain: "learning-firebase-7f42c.firebaseapp.com",
+	projectId: "learning-firebase-7f42c",
+	storageBucket: "learning-firebase-7f42c.appspot.com",
+	messagingSenderId: "942597330005",
+	appId: "1:942597330005:web:edb5ae5874ef38d28830dc",
 };
 
 // Initialize Firebase
